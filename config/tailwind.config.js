e4480002f69f9acbacd24aa8e2dfe4e3804d8a04
MC <chokamoses2@gmail.com>
2024-07-28 18:41:02 +0300

    updated puma

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         'poppins':["Poppins", "sans-serif"],
       },
       backgroundImage: {
-        'hero-image': "url(/assets/landing_hero.webp)",
+        'hero-image': "url(/assets/landing_hero.png)",
         'plan':"url(/assets/tic-tac-toe.png)",
       },
 
