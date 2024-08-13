8ad5637b0b30682d41833c3d9bddfc76670531ce
MC <chokamoses2@gmail.com>
2024-08-13 15:26:25 +0300

    added working markers on map

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,14 +7,18 @@ async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
     const map = new Map(document.getElementById("map"), {
-      center: { lat: 37.4239163, lng: -122.0947209 },
+      center: { lat: 0.0593318, lng: 34.2835138 },
       zoom: 14,
       mapId: "4504f8b37365c3d0",
+
     });
+
     const marker = new AdvancedMarkerElement({
-      map,
-      position: { lat: 37.4239163, lng: -122.0947209 },
+       map,
+       position: { lat: 0.0593318, lng: 34.2835138 },
     });
+    
+
   }
   
   initMap();
