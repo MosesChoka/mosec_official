7371bc3c25fd780e1be7aefa4219dbb751c609fd
MC <chokamoses2@gmail.com>
2024-08-13 14:46:35 +0300

    added markers on map

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,19 +1,14 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
 async function initMap() {
     // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
     const map = new Map(document.getElementById("map"), {
-      center: { lat: 37.4239163, lng: -122.0947209 },
+      center: { lat: 0.0593318, lng: 34.2835138 },
       zoom: 14,
-      mapId: "4504f8b37365c3d0",
     });
     const marker = new AdvancedMarkerElement({
       map,
-      position: { lat: 37.4239163, lng: -122.0947209 },
+      position: { lat: 0.0593318, lng: 34.2835138 },
     });
   }
   
