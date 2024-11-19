e24e04ed93118c2e9cde8214a19b292be6787abe
MC <chokamoses2@gmail.com>
2024-11-19 18:37:30 +0300

    removed authentication and subscription functions

--- spec/mailers/previews/newsletter_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/newsletter_mailer
-class NewsletterMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/newsletter_mailer/email_blast
-  def email_blast
-    NewsletterMailer.email_blast
-  end
-
-end
